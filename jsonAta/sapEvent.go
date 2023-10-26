88381583342bd840d1ea1dc34854e3917fa0bdcf
Romain Turpeau <54639402+turpeauR44@users.noreply.github.com>
2023-10-26 16:20:40 +0200

    Update sapEvent.go

--- jsonAta/sapEvent.go
+++ jsonAta/sapEvent.go
@@ -10,8 +10,25 @@ import (
 const jsonAtaExpr = `
     $.data.{
         "operationRequest":{
-        "id":manufacturingOrder
-	}
+        "id":manufacturingOrder,
+	"requestState":"ToBemapped",
+        "scheduledStartDate": mfgOrderScheduledStartDate &"T"& mfgOrderScheduledStartTime,
+        "scheduledEndDate": mfgOrderScheduledEndDate &"T"& mfgOrderScheduledEndTime,
+        "operationsType":"ToBemapped",
+        "segmentRequirment":{
+            "id":manufacturingOrder,
+            "earliestStartDate":"ToBeDefined",
+            "materialRequirements":{
+                "id":"SR."& manufacturingOrder &"."& productionOrderItems.material &".0?",
+                "quantity":mfgOrderPlannedTotalQty,
+                "materialLots":{
+                    "materialDefinition":{
+                        "id":material,
+                        "label":materialName
+                    }
+                }
+            }
+        }
     }
 `
 
