6e0daf81079a7f9dee6b781a3e1dc9deca042bf2
Romain Turpeau <rturpeau@4cad.fr>
2023-10-26 14:59:32 +0200

    jsonAtaTranform

--- JsonAta/sapEvent.go
+++ JsonAta/sapEvent.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"ioutil"
 
 	jsonata "github.com/blues/jsonata-go"
 )
@@ -15,6 +16,12 @@ const jsonAtaExpr = `
 	}
     }
 `
+type Event struct{
+	Topic string
+	Wait int
+	Qos int
+	Data map[string]interface{}
+}
 
 func TransformSAPOrderString(jsonString string) {
 
@@ -50,4 +57,17 @@ func TransformSAPOrder(data interface{}) {
 
 	fmt.Println(string(res))
 	// Output: 135
+}
+
+func TransformSAPOrder_test() {
+	content,err := ioutil.ReadFile("./samples/sap_order.json"+file.Name())
+	if err != nil{
+		log.Fatal("Error when opening file : ", err)
+	}
+	var file_event map[string]interface{}
+	err = json.Unmarshal(content,&file_event)
+	if err != nil{
+		log.Fatal("Error when UnWarshall: ", err)
+	}
+	TransformSAPOrder(file_event)
 }
